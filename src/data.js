6e827506247dc0f12870bd58942cb5684f69efe1
vv <89388361+vRyanv@users.noreply.github.com>
2022-10-13 09:31:41 +0700

    fix title login

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ const database = require("./database/connect");
  //        console.log(result.rows)
  //        data.end()
  //    })
-// data.query(`insert into users(username, password) values ('admin', '123456')`)
+// data.query(`insert into users(username, password) values ('admin2', '123123')`)
 //     .then((result) => {
 //         console.log(result)
 //         data.end()
@@ -13,20 +13,20 @@ const database = require("./database/connect");
 // data.query('select * from shop')
 //     .then(result => console.log(result))
 //     .then(data.end())
-// data.query('select * from shop')
-//     .then( (result) => {
-//         console.log(result)
-//         data.end()
-//     })
-let product = {
-    name: 'asdf',
-    cateId: 6,
-    supId: 1,
-    image: 'asdasd',
-    price: 1,
-    quantity: 1,
-}
-database.query(`insert into product(cate_id, shop_id, sup_id, pro_name, pro_image, pro_price, quantity)
-                                                 values (${product.cateId}, 12, ${product.supId},
-                                                         '${product.name}', '${product.image}',
-                                                         '${product.price}', ${product.quantity})`)
+data.query('select * from users')
+    .then( (result) => {
+        console.log(result)
+        data.end()
+    })
+// let product = {
+//     name: 'asdf',
+//     cateId: 6,
+//     supId: 1,
+//     image: 'asdasd',
+//     price: 1,
+//     quantity: 1,
+// }
+// database.query(`insert into product(cate_id, shop_id, sup_id, pro_name, pro_image, pro_price, quantity)
+//                                                  values (${product.cateId}, 12, ${product.supId},
+//                                                          '${product.name}', '${product.image}',
+//                                                          '${product.price}', ${product.quantity})`)
